e34d727cc4a34cd7ccff0afc29b4d26ebb746517
Kim-CheolHee <windowlife92@gmail.com>
2023-10-03 23:43:09 +0900

    Vue.js 시작하기 완료

--- vue-cli/src/App.vue
+++ vue-cli/src/App.vue
@@ -1,14 +1,25 @@
 <template>
     <div>
-        {{ str }}
+        <!-- <app-header v-bind:프롭스 속성 이름="상위 컴포넌트의 데이터 이름"></app-header> -->
+        <app-header :propsdata="str" @renew="renewStr"></app-header>
     </div>
 </template>
 
 <script>
+    import AppHeader from './components/AppHeader.vue'
+
     export default {
         data: function () {
             return {
                 str: 'Hi, Vue!'
+            }
+        },
+        components: {
+            'app-header': AppHeader
+        },
+        methods: {
+            renewStr: function () {
+                this.str = 'change Vue!'
             }
         }
     }
---
+++ vue-cli/src/components/AppHeader.vue
@@ -0,0 +1,22 @@
+<template>
+    <header>
+        <!-- <h1>Header</h1>     -->
+        <h1>{{ propsdata }}</h1>
+        <button @click="sendEvent">send</button>
+    </header>
+</template>
+
+<script>
+    export default {
+        props: ['propsdata'],
+        methods: {
+            sendEvent: function () {
+                this.$emit('renew')
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
